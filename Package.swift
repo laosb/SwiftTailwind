fe2fd639b09a989c511f278c0738902d46dcc6d5
laosb <git@lao.sb>
2026-01-20 21:29:17 +0800

    chore: bump deps.

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     .executable(name: "TailwindCSSCLIArtifactBundler", targets: ["TailwindCSSCLIArtifactBundler"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
-    .package(url: "https://github.com/apple/swift-crypto.git", from: "3.0.0"),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0"),
+    .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"5.0.0"),
   ],
   targets: [
     .executableTarget(
